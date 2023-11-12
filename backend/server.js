68d3be976e09aa40789c7d740df7aef89fa811b0
Muna Mussa <munammahdi@gmail.com>
2023-11-12 09:52:56 +0200

    setting up the fullstack repo

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
