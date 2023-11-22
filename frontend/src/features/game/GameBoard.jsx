a303477cc671fc0b5e131ce8e8ad58cad84d6965
Muna Mussa <munammahdi@gmail.com>
2023-11-22 11:45:43 +0200

    added gameboard

--- frontend/src/features/game/GameBoard.jsx
+++ frontend/src/features/game/GameBoard.jsx
@@ -1,4 +1,6 @@
+// src/features/game/GameBoard.jsx
 import React, { useState } from 'react';
+import GuessForm from '../../components/GuessForm'; // Assuming GuessForm is placed in the components directory
 
 function GameBoard() {
   const [guess, setGuess] = useState('');
@@ -6,14 +8,14 @@ function GameBoard() {
   const [attempts, setAttempts] = useState(0);
   const [correctNumber, setCorrectNumber] = useState(Math.floor(Math.random() * 100) + 1); // Random number between 1 and 100
 
-  const handleGuess = async () => {
-    setAttempts(prev => prev + 1);
-    // Backend call could go here to check the guess
-    // For now, we'll do it in the frontend
-    if (parseInt(guess) === correctNumber) {
+  const handleGuessSubmit = (userGuess) => {
+    const numGuess = parseInt(userGuess, 10);
+    setGuess('');
+    setAttempts((prevAttempts) => prevAttempts + 1); // Ensure attempts are incremented
+
+    if (numGuess === correctNumber) {
       setFeedback('Correct!');
-      // Here you could also handle what happens when the game is won
-    } else if (parseInt(guess) > correctNumber) {
+    } else if (numGuess > correctNumber) {
       setFeedback('Too high!');
     } else {
       setFeedback('Too low!');
@@ -28,19 +30,24 @@ function GameBoard() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <h2 className="text-3xl mb-4">Guess the Number</h2>
-      <input
-        type="number"
-        value={guess}
-        onChange={(e) => setGuess(e.target.value)}
-        className="mb-4 p-2 border-2 border-gray-300 rounded"
-        placeholder="Enter your guess..."
-      />
-      <button onClick={handleGuess} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">Submit Guess</button>
-      <p className="mt-4">{feedback}</p>
-      <p>Attempts: {attempts}</p>
-      {feedback === 'Correct!' && <button onClick={handleReset} className="mt-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700">Start New Game</button>}
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500">
+      <div className="bg-white max-w-md w-full rounded-lg shadow-2xl p-8 m-4">
+        <h2 className="text-3xl text-center font-bold mb-8">Guess the Number</h2>
+        <GuessForm onGuessSubmit={handleGuessSubmit} attempts={attempts} />
+        {feedback && (
+          <div className={`mt-4 p-4 text-center font-bold ${feedback === 'Correct!' ? 'text-green-500' : 'text-red-500'}`}>
+            {feedback}
+          </div>
+        )}
+        {feedback === 'Correct!' && (
+          <button
+            onClick={handleReset}
+            className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg transition duration-300 ease-in-out"
+          >
+            Start New Game
+          </button>
+        )}
+      </div>
     </div>
   );
 }
